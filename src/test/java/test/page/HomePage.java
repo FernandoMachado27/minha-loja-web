afffb284b554b587ae0ae3f763eee504a6a06f8f
FernandoMachado27 <fernando2747@yahoo.com>
2024-03-05 23:43:32 -0300

    Ler configurações do arquivo de propriedades - Configuration.properties e ConfigFileReader

--- src/test/java/test/page/HomePage.java
+++ src/test/java/test/page/HomePage.java
@@ -3,9 +3,12 @@ package test.page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import core.dataProviders.ConfigFileReader;
+
 public class HomePage {
 	
 	WebDriver driver;
+	ConfigFileReader configFileReader;
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
--- src/test/java/test/steps/HomeSteps.java
+++ src/test/java/test/steps/HomeSteps.java
@@ -2,6 +2,7 @@ package test.steps;
 
 import org.openqa.selenium.WebDriver;
 
+import core.dataProviders.ConfigFileReader;
 import core.managers.PageObjectManager;
 import test.page.HomePage;
 
@@ -10,5 +11,6 @@ public class HomeSteps {
 	WebDriver driver;
 	HomePage homePage;
 	PageObjectManager pageObjectManager;
+	ConfigFileReader configFileReader;
 
 }
